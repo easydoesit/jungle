91a5c90fd46bc2cccd1eeefd2fd259b84c31ac2c
easydoesit <michael.grills@gmail.com>
2023-04-10 17:04:50 +0000

    add authorization concern and modules to admin/dashboard controller and admin/products controller

---
+++ app/models/concerns/http_auth_concern.rb
@@ -0,0 +1,16 @@
+module HttpAuthConcern
+  extend ActiveSupport::Concern
+
+  included do
+     before_action :http_authenticate
+  end
+
+  def http_authenticate
+  
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_NAME'] && password = ENV['BASIC_AUTH_PASSWORD']
+    end
+  
+  end
+
+end
