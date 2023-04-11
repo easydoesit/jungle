20f7a3e7717b6aeb795ce679fbe36019f1b853aa
easydoesit <michael.grills@gmail.com>
2023-04-11 03:41:19 +0000

    fix merge issue

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  
+  include HttpAuthConcern
+  
   def show    
     @product_count = Product.count
     @category_count = Category.count
   
-  include HttpAuthConcern
-  
-  def show
   end
 
 end
